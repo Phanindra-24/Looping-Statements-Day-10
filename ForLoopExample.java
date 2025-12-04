dc0cb1cd82cf47ff6e9bf33859799dccdadcfa85
Phanindra-24 <anusuriphanindra@gmail.com>
2025-12-04 20:02:15 +0530

    Add files via upload

---
+++ ForLoopExample.java
@@ -0,0 +1,14 @@
+package com.codegnan.controlstatemnts;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for(System.out.println("hi"); i <= 10;System.out.println("Hello")) {
+			i++;
+			
+		}
+
+	}
+
+}
